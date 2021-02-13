f65433e1d8ce3afbad0bd25d7e2ca470e5d2d218
Shane Sutro <shanesutro@gmail.com>
2021-02-13 16:20:06 -0700

    Cred location change

--- database/firestore.py
+++ database/firestore.py
@@ -6,8 +6,7 @@ import os
 print(os.path.dirname(os.path.realpath(__file__)))
 
 # cred = credentials.Certificate(os.path.dirname(os.path.realpath(__file__)) + '/cred.json')
-cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app()
 
 db = firestore.client()
 
